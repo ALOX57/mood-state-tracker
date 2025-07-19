a2881038398745f8aed19cfadc7866b004b3f863
Alexander Birtles <aloxemail@gmail.com>
2025-07-19 18:02:09 +1000

    refactor: fully removed json logging in favor for sql

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 # main.py
 
 from datetime import datetime
-import json
-import os
-import shutil
 import sqlite3
 
 print(datetime.now().isoformat())
@@ -11,56 +8,11 @@ print(datetime.now().isoformat())
 mood = input("Mood (1-10 or label): ").strip()
 print("You entered:", mood)
 
-DATA_FILE = 'data/logs.json'
 
 entry = {
     "timestamp": datetime.now().isoformat(),
     "mood": mood
 }
-
-
-# Load existing logs - or empty list if file is new/broken
-if not os.path.exists(DATA_FILE):
-    logs = []
-else:
-    with open(DATA_FILE, 'r') as f:
-        try:
-            logs = json.load(f)
-        except json.JSONDecodeError:
-            logs = []
-
-logs.append(entry)
-
-# with open(DATA_FILE, 'w') as f:
-#     json.dump(logs, f, indent=2)
-
-# Writing to temp file
-TEMP_FILE = 'data/logs_temp.json'
-
-with open(TEMP_FILE, 'w') as f:
-    json.dump(logs, f, indent=2)
-    f.flush()
-    os.fsync(f.fileno())
-
-# Validate
-with open(TEMP_FILE, 'r') as f:
-    try:
-        test = json.load(f)
-        assert isinstance(test, list)
-        for item in test:
-            assert isinstance(item, dict)
-            assert "mood" in item
-            assert "timestamp" in item
-    except Exception:
-        print("ERROR: Temp file corrupted - not replacing original!")
-        exit()
-shutil.move(TEMP_FILE, DATA_FILE)
-
-
-print("Saved.")
-
-
-
 
 DB_FILE = 'data/logs.db'
 conn = sqlite3.connect(DB_FILE)
@@ -81,3 +33,5 @@ VALUES (?, ?)
 
 conn.commit()
 conn.close()
+
+print("Saved.")
